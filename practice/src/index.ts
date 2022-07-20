28f15b34671e9780bb2e6b24efa2c7a9f804ffdb
Yusuke Hiraune <y_hiraune@wts-c.co.jp>
2022-07-20 11:32:39 +0900

    feat: practice to add class

--- practice/src/index.ts
+++ practice/src/index.ts
@@ -821,6 +821,437 @@ practice("# 関数", () => {
   });
 });
 
+practice("# クラス", () => {
+  practice("## クラス宣言", () => {
+    class User {
+      name: string = "";
+      //    name?: string = "";
+      //    readonly name: string = "";
+      age: number = 0;
+
+      isAdult(): boolean {
+        return this.age >= 20;
+      }
+
+      setAge(newAge: number) {
+        this.age = newAge;
+      }
+    }
+
+    const uhyo = new User();
+    console.log(uhyo.name);
+    console.log(uhyo.age);
+
+    //  uhyo.age = 26;
+    //  console.log(uhyo.age);
+
+    console.log(uhyo.isAdult());
+    uhyo.setAge(26);
+    console.log(uhyo.isAdult());
+
+    practice("### コンストラクタ 静的プロパティ 静的メソッド", () => {
+      class User {
+        static adminName: string = "uhyo";
+        static getAdminUser() {
+          return new User(User.adminName, 26);
+        }
+
+        name: string;
+        readonly age: number;
+
+        constructor(name: string, age: number) {
+          this.name = name;
+          this.age = age;
+        }
+
+        isAdult(): boolean {
+          return this.age >= 20;
+        }
+      }
+
+      console.log(User.adminName);
+      const admin = User.getAdminUser();
+      console.log(admin.age);
+      console.log(admin.isAdult());
+
+      const uhyo = new User("uhyo", 26);
+      console.log(uhyo.name);
+      console.log(uhyo.age);
+    });
+
+    practice("### アクセシビリティ修飾子", () => {
+      class User {
+        name: string;
+        private age: number;
+
+        constructor(name: string, age: number) {
+          this.name = name;
+          this.age = age;
+        }
+
+        isAdult(): boolean {
+          return this.age >= 20;
+        }
+      }
+
+      const uhyo = new User("uhyo", 26);
+      console.log(uhyo.name);
+      console.log(uhyo.isAdult());
+    });
+
+    practice("### コンストラクタ引数でのプロパティ宣言", () => {
+      class User {
+        // TypeScript 特有の構文
+        constructor(public name: string, private age: number) {}
+      }
+    });
+
+    practice("### クラス式", () => {
+      const User = class {
+        name: string;
+        age: number;
+
+        constructor(name: string, age: number) {
+          this.name = name;
+          this.age = age;
+        }
+
+        isAdult(): boolean {
+          return this.age >= 20;
+        }
+      };
+      const uhyo = new User("uhyo", 26);
+      console.log(uhyo.name);
+      console.log(uhyo.isAdult());
+    });
+
+    practice("### もう一つのプライベートなプロパティ", () => {
+      class User {
+        name: string;
+        // JavaScript 由来でランタイム時にも有効なプライベート
+        #age: number;
+
+        constructor(name: string, age: number) {
+          this.name = name;
+          this.#age = age;
+        }
+
+        isAdult(): boolean {
+          return this.#age >= 20;
+        }
+      }
+
+      const uhyo = new User("uhyo", 26);
+      console.log(uhyo.name);
+      console.log(uhyo.isAdult());
+    });
+
+    practice("### 静的初期化ブロック", () => {
+      class User {
+        static adminUser: User;
+        static {
+          this.adminUser = new User();
+          this.adminUser.#age = 9999;
+        }
+
+        #age: number = 0;
+        getAge() {
+          return this.#age;
+        }
+        setAge(age: number) {
+          if (age < 0 || age > 150) {
+            return;
+          }
+          this.#age = age;
+        }
+      }
+      console.log(User.adminUser.getAge());
+    });
+
+    practice("### 型引数を持つクラス", () => {
+      class User<T> {
+        name: string;
+        #age: number;
+        readonly data: T;
+
+        constructor(name: string, age: number, data: T) {
+          this.name = name;
+          this.#age = age;
+          this.data = data;
+        }
+
+        public isAdult(): boolean {
+          return this.#age >= 20;
+        }
+      }
+
+      const uhyo = new User<string>("uhyo", 26, "追加データ");
+      const data = uhyo.data;
+      console.log(data);
+
+      const john = new User("John Smith", 15, { num: 123 });
+      const data2 = john.data;
+      console.log(data2);
+    });
+  });
+
+  practice("## クラスの型", () => {
+    class User {
+      name: string = "";
+      age: number = 0;
+
+      isAdult(): boolean {
+        return this.age >= 20;
+      }
+    }
+
+    const uhyo: User = new User();
+    const john: User = {
+      name: "John Smith",
+      age: 15,
+      isAdult: () => true,
+    };
+    console.log(uhyo);
+    console.log(john);
+
+    practice("### newシグネチャによるインスタンス可能性の表現", () => {
+      class User {
+        name: string = "";
+        age: number = 0;
+      }
+
+//      type MyUserConstructor = new () => User;
+      type MyUserConstructor = {
+        new (): User;
+      };
+
+      const MyUser: MyUserConstructor = User;
+      const u = new MyUser();
+      console.log(u.name, u.age);
+    });
+
+    practice("### instanceof", () => {
+      class User {
+        name: string = "";
+        age: number = 0;
+      }
+
+      const uhyo = new User();
+      console.log(uhyo instanceof User);
+      console.log({} instanceof User);
+
+      const john: User = {
+        name: "John Smith",
+        age: 15,
+      };
+      // User クラスのインスタンスかどうかを判定しているため new User を指さないと false
+      console.log(john instanceof User);
+
+      practice("#### 型の絞込み", () => {
+        type HasAge = {
+          age: number;
+        }
+        class User {
+          name: string = "";
+          age: number = 0;
+  
+          constructor(name: string, age: number) {
+            this.name = name;
+            this.age = age;
+          }
+        }
+        
+        function getPrice(customer: HasAge) {
+          if (customer instanceof User && customer.name === "uhyo") {
+            return 0;
+          }
+          return customer.age < 18 ? 1000: 1800;
+        }
+
+        const customer1: HasAge = { age: 15 };
+        const customer2: HasAge = { age: 40 };
+        const uhyo = new User("uhyo", 26);
+
+        console.log(getPrice(customer1));
+        console.log(getPrice(customer2));
+        console.log(getPrice(uhyo));
+      });
+    });
+  });
+
+  practice("## クラスの継承", () => {
+    class User {
+      name: string;
+      #age: number;
+
+      constructor(name: string, age: number) {
+        this.name = name;
+        this.#age = age;
+      }
+
+      public isAdult(): boolean {
+        return this.#age >= 20;
+      }
+    }
+
+    class PremiumUser extends User {
+      rank: number = 1;
+
+      constructor(name: string, age: number, rank: number) {
+        // this にアクセスするより先に super を呼び出す必要がある
+        super(name, age);
+        this.rank = rank;
+      }
+
+      // noImplicitOverride コンパイラオプションと組み合わせると付け忘れや意図しないオーバーライドをコンパイルエラーで検知できる
+      public override isAdult(): boolean {
+        return true;
+      }
+    }
+
+    function getMessage(u: User) {
+      return `こんにちは、${u.name}さん`;
+    }
+
+    const john = new User("John Smith", 15);
+    const uhyo = new PremiumUser("uhyo", 26, 3);
+    console.log(uhyo.rank);
+    console.log(uhyo.name);
+    console.log(uhyo.isAdult());
+    console.log(getMessage(john));
+    console.log(getMessage(uhyo));
+
+    practice("### implements", () => {
+      type HasName = {
+        name: string;
+      }
+
+      class User implements HasName {
+        name: string;
+        #age: number;
+
+        constructor(name: string, age: number) {
+          this.name = name;
+          this.#age = age;
+        }
+
+        public isAdult(): boolean {
+          return this.#age >= 20;
+        }
+      }
+    });
+  });
+
+  practice("## this", () => {
+    const user = {
+      name: "uhyo",
+      age: 26,
+      isAdult() {
+        return this.age >= 20;
+      }
+    };
+
+    // isAdult() と呼び出すとオブジェクトがなく undefined になってしまうため、メソッド記法で呼び出すことを推奨
+    console.log(user.isAdult());
+    user.age = 15;
+    console.log(user.isAdult());
+
+    practice("### アロー関数における this", () => {
+      class User {
+        name: string;
+        #age: number;
+
+        constructor(name: string, age: number) {
+          this.name = name;
+          this.#age = age;
+        }
+
+        public isAdult(): boolean {
+          return this.#age >= 20;
+        }
+
+        public filterOlder(users: readonly User[]): User[] {
+          // 外側の関数から this を受け継ぐアロー関数を推奨
+          return users.filter(u => u.#age > this.#age);
+        }
+      }
+
+      const uhyo = new User("uhyo", 25);
+      const john = new User("John Smith", 15);
+      const bob = new User("Bob", 40);
+
+      const older = uhyo.filterOlder([john, bob]);
+      console.log(older);
+    });
+  });
+
+  practice("## 例外", () => {
+    try {
+      console.log("エラーを発生させます");
+      throwError();
+      console.log("エラーを発生させました");
+    } catch (err) {
+      console.log("エラーをキャッチさせました");
+      console.log(err);
+    } finally {
+      console.log("finallyブロック");
+    }
+    console.log("おわり");
+
+    function throwError() {
+      const error = new Error("エラーが発生しました！！！！！");
+      throw error;
+    }
+
+    try {
+      throwNull();
+    } catch (err) {
+      console.log(err, "が投げられました");
+    }
+
+    function throwNull() {
+      // エラー以外でなんでも投げられる
+      throw null;
+    }
+  });
+
+  practice("## object to class", () => {
+    class User {
+      readonly name: string;
+      readonly age: number;
+
+      constructor(name: string, age: number) {
+        if (name === "") {
+          throw new Error("名前は空にできません！");
+        }
+        this.name = name;
+        this.age = age;
+      }
+
+      getMessage(message: string): string {
+        return `${this.name} (${this.age}) 「${message}」`;
+      }
+    }
+
+    const uhyo = new User("uhyo", 26);
+    console.log(uhyo.getMessage("こんにちは"));
+
+    practice("### Closure", () => {
+      function createUser(name: string, age: number) {
+        if (name === "") {
+          throw new Error("名前は空にできません！");
+        }
+        return (message: string) => {
+          return `${name} (${age}) 「${message}」`;
+        };
+      }
+  
+      const getMessage = createUser("uhyo", 26);
+      console.log(getMessage("こんにちは"));  
+    });
+  });
+});
+
 practice("# Read line", () => {
   function getDefaultName() {
     return "名無し";
